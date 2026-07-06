f2c0aeba0f93467306ec7a95feeff3ac160d2f89
riii111 <riii111@users.noreply.github.com>
2026-07-06 14:00:02 +0900

    chore: bootstrap rust development setup

---
+++ flake.nix
@@ -0,0 +1,100 @@
+{
+  description = "wezterm-git-status-bridge development and build environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+    }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+
+      rustVersion = "1.96.0";
+      mkRustToolchain =
+        pkgs:
+        pkgs.rust-bin.stable.${rustVersion}.default.override {
+          extensions = [
+            "clippy"
+            "rust-analyzer"
+            "rust-src"
+            "rustfmt"
+          ];
+        };
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ rust-overlay.overlays.default ];
+          };
+
+          rustToolchain = mkRustToolchain pkgs;
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = rustToolchain;
+            rustc = rustToolchain;
+          };
+        in
+        {
+          default = rustPlatform.buildRustPackage {
+            pname = "wezterm-git-status-bridge";
+            version = "0.1.0";
+
+            src = self;
+            cargoLock.lockFile = ./Cargo.lock;
+
+            meta = {
+              description = "Bridge Git repository status into WezTerm status rendering";
+              homepage = "https://github.com/riii111/wezterm-git-status-bridge";
+              license = pkgs.lib.licenses.mit;
+              mainProgram = "wezterm-git-status-bridge";
+            };
+          };
+        }
+      );
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ rust-overlay.overlays.default ];
+          };
+
+          rustToolchain = mkRustToolchain pkgs;
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              rustToolchain
+              pkgs.cargo-audit
+              pkgs.cargo-machete
+              pkgs.lefthook
+            ];
+          };
+        }
+      );
+
+      formatter = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.nixfmt
+      );
+    };
+}
